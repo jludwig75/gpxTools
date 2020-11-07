da04b8d4214e2b9bc44b4e90fda4baeadd990869
Jonathan Ludwig <jr.ludwig@gmail.com>
2020-11-07 13:50:01 -0700

    Collapse parser

--- gpxserver/parser.h
+++ gpxserver/parser.h
@@ -5,15 +5,13 @@
 
 #include "gpxtools.grpc.pb.h"
 
-#include "gpxparser.h"
-
 
 class ParserImpl final : public gpxtools::Parser::Service
 {
 public:
+    ParserImpl();
+    ~ParserImpl();
     grpc::Status parseFile(grpc::ServerContext* context,
                            grpc::ServerReader<gpxtools::GpxDataChunk>* reader,
                            gpxtools::Activity* activity) override;
-private:
-    gpx::Parser _parser;
 };
